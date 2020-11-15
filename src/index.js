d6373df7eb7beb67c69be8339e2b1294b354717b
wtwerner <wtwerner@gmail.com>
2020-11-15 11:19:59 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,46 @@
-console.log('%c HI', 'color: firebrick')
+document.addEventListener("DOMContentLoaded", function() {
+    fetchImages();
+    fetchBreeds();
+})
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+const breedUrl = "https://dog.ceo/api/breeds/list/all"
+const dropdown = document.querySelector('#breed-dropdown')
+
+function fetchImages() {
+    fetch(imgUrl)
+    .then(resp => resp.json())
+    .then(json => json.message.forEach(image => renderImage(image)))
+}
+  
+function renderImage(image) {
+    const img_cont = document.getElementById('dog-image-container')
+    let newImg = document.createElement('img')
+    newImg.src = image
+    img_cont.appendChild(newImg)
+}
+
+function fetchBreeds() {
+    fetch(breedUrl)
+    .then(resp => resp.json())
+    .then(json => {
+        let breeds = Object.keys(json.message);
+        renderBreeds(breeds)
+    }) 
+}
+
+dropdown.addEventListener('change', function(e) {
+    selectBreeds(e.target.value)
+})
+
+function renderBreeds(breeds) {
+    breeds.forEach(breed => {
+        let list = document.getElementById('dog-breeds')
+        let li = document.createElement('li')
+        li.innerText = breed
+        li.addEventListener('click', function(e) {
+            e.target.style.color = 'red'
+        })
+        list.appendChild(li)
+    })
+}
