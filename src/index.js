0f4df9929e7d88865d693844a2bc0bfb26b08df3
danielkwon89 <danielkwon89@gmail.com>
2020-11-17 21:25:06 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-console.log('%c HI', 'color: firebrick')
+// console.log('%c HI', 'color: firebrick')
 
 window.addEventListener('DOMContentLoaded', () => {
     fetchImg();
@@ -8,19 +8,17 @@ const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
 
 const breedUrl = 'https://dog.ceo/api/breeds/list/all'
 
-let dogImageContainer = document.getElementById('dog-image-container');
-
 function fetchImg(){
     return fetch("https://dog.ceo/api/breeds/image/random/4")
     .then(resp => resp.json())
-    // .then(json => renderImg(json));
+    .then(json => renderImg(json));
 };
 
 function renderImg(image) {
-    console.log(image);
-//   image.forEach(image => {
-//       let element = document.createElement('img');
-//       element.src = image;
-//       dogImageContainer.appendChild(element);
-//   })
+  image.message.forEach(image => {
+      let dogImageContainer = document.getElementById('dog-image-container');
+      let element = document.createElement('img');
+      element.src = image;
+      dogImageContainer.appendChild(element);
+  })
 }
