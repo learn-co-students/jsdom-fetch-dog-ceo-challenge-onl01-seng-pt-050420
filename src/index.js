b299fa699f5c87ac21eaa24017d0989c5bc7d31d
Jonathan East <jonathan_east@ymail.com>
2020-11-18 15:04:21 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
 console.log('%c HI', 'color: firebrick')
+
+let breeds = [];
+
+document.addEventListener("DOMContentLoaded", function () {
+  getImages();
+  getBreedOptions();
+});
+
+function getImages() {
+  const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+  fetch(imgUrl)
+    .then(resp => resp.json())
+    .then(result => { 
+      result.message.forEach(image => addImage(image))
+    });
+}
+
+function addImage(dogImgUrl) {
+  let container = document.querySelector('#dog-image-container');
+  let createElement = document.createElement('img');
+  createElement.src = dogImgUrl;
+  container.appendChild(createElement);
+}
+
+
