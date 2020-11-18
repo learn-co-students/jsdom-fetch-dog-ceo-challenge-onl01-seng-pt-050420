b35c2f447008499a9ae04381c851db2cc009193b
Zachary Lynch <zachary.lynch1337@gmail.com>
2020-11-18 19:16:56 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
 console.log('%c HI', 'color: firebrick')
+
+function fetchChallengeOne() {
+    fetch("https://dog.ceo/api/breeds/image/random/4")
+    .then(res => {
+        return res.json();
+    })
+    .then (json => {
+        for (let i = 0; i < json.message.length; i++) {
+            console.log(json.message[i]);
+            let img = document.createElement('img');
+            img.src = json.message[i];
+            let src = document.getElementById("dog-image-container");
+            src.appendChild(img);
+        }
+    });
+}
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    fetchChallengeOne();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
