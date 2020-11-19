81b2ebcb449ce2ed5c85f49786e01f10bc996c07
danielkwon89 <danielkwon89@gmail.com>
2020-11-18 22:15:22 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
-// Challenge 1
-
 window.addEventListener('DOMContentLoaded', () => {
     fetchImg();
+    fetchBreed();
 });
+
+
+// Challenge 1
 
 const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
 
 function fetchImg(){
-    return fetch("https://dog.ceo/api/breeds/image/random/4")
+    return fetch(imgUrl)
     .then(resp => resp.json())
     .then(json => renderImg(json));
 };
 
-function renderImg(image) {
+function renderImg(image){
   image.message.forEach(image => {
       let dogImageContainer = document.getElementById('dog-image-container');
       let element = document.createElement('img');
@@ -25,3 +27,23 @@ function renderImg(image) {
 // Challenge 2
 
 const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+
+function fetchBreed(){
+    return fetch(breedUrl)
+    .then(resp => resp.json())
+    .then(json => renderBreed(json));
+};
+
+function renderBreed(breed){
+    console.log(breed.message);
+    Object.keys(breed.message).forEach(breed => {
+        let breedContainer = document.getElementById('dog-breeds');
+        let element = document.createElement('li');
+        element.innerText = breed; 
+        breedContainer.appendChild(element);
+    })
+}
+
+
+// Challenge 3
+
